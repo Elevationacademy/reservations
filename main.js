13ed7978992789979171149dd1d0304e03e84633
nitzan cohen <nitzancohen105@gmail.com>
2018-08-16 22:56:19 +0300

    wrote claimReservation function

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
-var reservations = {
+const reservations = {
   'Bob': { claimed: false },
   'Ted': { claimed: true }
 }
 
-var name = prompt('Please enter the name for your reservation');
+const name = prompt('Please enter the name for your reservation');
 
-var claimReservation = function () {
+const claimReservation = function () {
   // write your code here!
+  if (hasReservasion(name)) {
+    if (reservations[name].claimed) {
+      alert("You have already claimed your reservation")
+    } else {
+      alert("Welcome, " + name)
+      reservations[name].claimed = true
+    }
+  } else {
+    alert("Sorry, no reservations under that name..")
+  }
+}
+
+function hasReservasion (name) {
+  const names = Object.keys(reservations) // ['Bob', 'Ted']
+  for (let n of names) {
+    if (name == n) {
+      return true
+    }
+  }
+  return false
 }
 
 claimReservation();
